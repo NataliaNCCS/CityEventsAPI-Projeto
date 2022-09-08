2a3e0db3053f8e1670cee6e5c31b6e766529a727
Natália Campos <nataliacamposnccs@gmail.com>
2022-09-08 17:39:52 -0300

    Add project files.

---
+++ CityEventsAPI/Controllers/CityEventsController.cs
@@ -0,0 +1,20 @@
+﻿using CityEventsAPI.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityEventsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+
+    public class CityEventsController
+    {
+        //public ICityEventService _cityEventService;
+
+        //public CityEventController(ICityEventService cityEventService)
+        //{
+        //    _cityEventService = cityEventService;
+        //}
+    }
+}
